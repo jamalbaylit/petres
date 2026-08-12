50874c73c3b0f1b7b6cd6a18f8b36f0b68977ec7
Tayfun Jamalbayli <60936247+jamalbaylit@users.noreply.github.com>
2026-08-12 10:46:52 +0300

    wells removed from import (#497)
    * wells removed from import
    
    * tests are fixed

--- examples/grid/from_wells.py
+++ examples/grid/from_wells.py
@@ -1,6 +1,6 @@
 """ Create a grid from well data """
 
-from petres.models import VerticalWell, Horizon, Zone, wells
+from petres.models import VerticalWell, Horizon, Zone
 from petres.grids import CornerPointGrid, PillarGrid
 from petres.interpolators import IDWInterpolator
 from petres.viewers import Viewer3D 
